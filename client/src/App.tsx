152eed249766e3931a347cf38acdea7330e513f5
VividTruthKeeper <mr.aydogdy02@gmail.com>
2022-09-09 23:32:30 +0500

    project initialized, mixins & variables

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,42 +1,8 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+// Styles
+import "./assets/styles/style.scss";
 
-function App() {
-  const [count, setCount] = useState<number>(0);
-  const [data, setData] = useState<string>("");
-  useEffect(() => {
-    axios.get("/").then((res) => {
-      setData(res.data);
-    });
-  }, []);
-
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        <h1>{data}</h1>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  );
-}
+const App = () => {
+  return <div className="App"></div>;
+};
 
 export default App;
