b04c0c89e89ef776eb502849bb70682a8fc04b9a
VividTruthKeeper <mr.aydogdy02@gmail.com>
2022-09-18 23:07:02 +0500

    comment unused code

--- server/src/controllers/user/auth.controller.ts
+++ server/src/controllers/user/auth.controller.ts
@@ -1,10 +1,10 @@
 // Modules
 import bcrypt from "bcryptjs";
-import expressAsyncHandler from "express-async-handler";
-import { v4 as uuidv4 } from "uuid";
+// import expressAsyncHandler from "express-async-handler";
+// import { v4 as uuidv4 } from "uuid";
 
 // Salty hash
-const hashPassword = (password: string): string => {
+export const hashPassword = (password: string): string => {
   const salt = bcrypt.genSaltSync(10);
   const hash = bcrypt.hashSync(password, salt);
   return hash;
